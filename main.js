fb5ae6a5f8ed2b02356d2c0236657ab9932ccf12
Данил Янзин <danilyanzin@MacBook-Pro-Danil.local>
2023-06-19 18:15:14 +0300

    fist commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const load = document.querySelector('#loadButton')
+const result = document.querySelector('#result')
+
+load.addEventListener('click', (e) => {
+    e.preventDefault();
+    result.innerText = 'click'
+})
+
+
